4d8515c582d741212919e4192d5c14f50d722856
Kasia <kasia@miroartdesign.com>
2020-02-04 18:03:55 -0500

    api calls error

--- src/Services/APIcalls.js
+++ src/Services/APIcalls.js
@@ -1,23 +1,25 @@
 import React from 'react';
-
+import config from '../config';
 
 
 const APIcalls = {
-    getAllRestaurants: () => {
-        const URL = config.API_ENDPOINT 
-
+    getAllRestaurants: (term, location) => {
+       
+        const URL = config.API_ENDPOINT + `term=${term}` +'&'+ `location=${location}`+'&callback=myCallback';
+        console.log(URL)
         return fetch(URL, {
             headers: {
+                'Access-Control-Allow-Origin': '*',
                 'Authorization': `Bearer ${config.API_KEY}`
             },
         })
             .then(res => {
                 if (!res.ok) {
-                   return res.json()
-                   .then(err=> {
-                       console.log(err)
-                       throw new Error(err.error.message)
-                   })
+                    return res.json()
+                        .then(err => {
+                            console.log(err)
+                            throw new Error(err.error.message)
+                        })
                 }
                 console.log(res)
                 return res;
@@ -25,3 +27,4 @@ const APIcalls = {
             .then(res => res.json())
     }
 }
+export default APIcalls;
--- src/SmallForm/SmallForm.js
+++ src/SmallForm/SmallForm.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import StatesSelect from './StatesSelect';
-
+import APIcalls from '../Services/APIcalls'
 export default class SmallForm extends React.Component {
+constructor(){
+    super()
+    this.state = {
+        error: null,
+        stateSelection: null,
+        city: null,
+    }
+}
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        const location = this.state.city;
+        const term = 'restaurants';
+        APIcalls.getAllRestaurants(term, location)
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            this.setState({
+                error: err
+            })
+        })
+    }
+    
+    handleCityInput = (e)=> {
+        e.preventDefault();
+        const location = e.target.value;
+        this.setState({
+            city: location,
+        })
+    }
+
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleFormSubmit}>
                 <h2>Search restaurants in : </h2>
                 <label>City</label>
-                <input type='text' />
+                <input type='text' onChange={this.handleCityInput}/>
                 <StatesSelect /> 
                <br />
                 <button>Search</button>
