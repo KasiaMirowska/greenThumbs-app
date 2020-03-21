b3acd68a303deb1404e594476358088958989f02
Kasia <kasia@miroartdesign.com>
2020-03-20 23:04:55 -0400

    d

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://agile-bayou-12370.herokuapp.com/' || 'http://localhost:8000/api',
     API_KEY: process.env.REACT_API_KEY,
 }
