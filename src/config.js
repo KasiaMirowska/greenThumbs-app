4e1631ac71c072d1592e4c9417cab41463770731
Kasia <kasia@miroartdesign.com>
2020-04-14 16:58:24 -0400

    minor improvments after deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://agile-bayou-12370.herokuapp.com/api',
+    API_ENDPOINT: 'https://green-thumbs-up.herokuapp.com/api',
     API_KEY: process.env.REACT_API_KEY,
 }
